49adbde9dd721498073fb88f5013e9514f550f5f
DELETRAZ Octavio <octavio.deletraz@gmail.com>
2022-08-16 12:59:32 -0300

    segunda clase

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from "../logo.svg";
+
+function NavBar() {
+
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <img
+                    src={logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="React Bootstrap logo"
+                />
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBar;
