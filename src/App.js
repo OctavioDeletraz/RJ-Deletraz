4b57e9799bf58995d85d009722050b629ecb44f2
DELETRAZ Octavio <octavio.deletraz@gmail.com>
2022-09-01 16:38:33 -0300

    Se agrega routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import React from 'react';
 import NavBar from './components/navbar/NavBar'
 import ItemListContainer from './components/itemListContainer/ItemListContainer';
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+
+      <Routes>
+        {/* Listado de rutas */}
+        <Route path="contacto" element={<contacto />} />
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes>
+
       <NavBar />
       <ItemListContainer titulo="MI TIENDA ONLINE" />
-    </div>
+      {/* <Footer/> */}
+    </BrowserRouter>
   );
 }
 
