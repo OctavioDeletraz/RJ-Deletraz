2ef4cd999f94374052cc96cfeb36949a8e8dc0b6
DELETRAZ Octavio <octavio.deletraz@gmail.com>
2022-09-26 16:27:53 -0300

    1

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -1,12 +1,23 @@
 import { Button } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 export const CheckOut = () => {
+
+    const [nombre, setNombre] = useState()
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault()
+        const orden = {
+
+        }
+    }
+
     return (
         <div>
             <h2>CheckOut</h2>
             <hr />
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type={'text'} placeholder="Tu nombre" />
                 <input type={'email'} placeholder="Email" />
                 <input type={'text'} placeholder="Dirección" />
